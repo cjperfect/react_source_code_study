cbc02432442f6e7327af116bcb58781133bd5ce5
陈江 <chenjiang@ysstech.com>
2022-08-25 14:26:36 +0800

    格式化

--- webpack.config.js
+++ webpack.config.js
@@ -2,50 +2,47 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 const webpack = require("webpack");
 module.exports = {
-	entry: "./src/index.js",
-	mode: "development",
-	devtool: "source-map",
-	devServer: {
-		port: 9000,
-	},
-	module: {
-		rules: [
-			{
-				test: /.(js)|(jsx)$/,
-				use: {
-					loader: "babel-loader",
-					options: {
-						presets: ["@babel/preset-flow"],
-					},
-				},
-			},
-		],
-	},
-	devServer: {
-		port: "9999",
-		host: "localhost",
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			template: "./public/index.html",
-		}),
-		new webpack.DefinePlugin({
-			__DEV__: true,
-			__PROFILE__: true,
-			__EXPERIMENTAL__: true,
-			__UMD__: true,
-		}),
-	],
-	resolve: {
-		alias: {
-			react: path.resolve(__dirname, "./packages/react"),
-			shared: path.resolve(__dirname, "./packages/shared"),
-			scheduler: path.resolve(__dirname, "./packages/scheduler"),
-			"react-dom": path.resolve(__dirname, "./packages/react-dom"),
-			"react-reconciler": path.resolve(
-				__dirname,
-				"./packages/react-reconciler"
-			),
-		},
-	},
+  entry: "./src/index.js",
+  mode: "development",
+  devtool: "source-map",
+  devServer: {
+    port: 9000
+  },
+  module: {
+    rules: [
+      {
+        test: /.(js)|(jsx)$/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-flow"]
+          }
+        }
+      }
+    ]
+  },
+  devServer: {
+    port: "9999",
+    host: "localhost"
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html"
+    }),
+    new webpack.DefinePlugin({
+      __DEV__: true,
+      __PROFILE__: true,
+      __EXPERIMENTAL__: true,
+      __UMD__: true
+    })
+  ],
+  resolve: {
+    alias: {
+      react: path.resolve(__dirname, "./packages/react"),
+      shared: path.resolve(__dirname, "./packages/shared"),
+      scheduler: path.resolve(__dirname, "./packages/scheduler"),
+      "react-dom": path.resolve(__dirname, "./packages/react-dom"),
+      "react-reconciler": path.resolve(__dirname, "./packages/react-reconciler")
+    }
+  }
 };
